c6cb5aac9cbbcccdbd826a72c6ddb86223ecb9ad
sdew021 <44828726+sdew021@users.noreply.github.com>
2020-01-29 23:50:35 +0530

    Update test1.c

--- lexicalAnalyzer/test1.c
+++ lexicalAnalyzer/test1.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
-#define x 3
 void main()
 {
-	int a=3;
-	float b=5.6;
-	while(a<20)
-	{
-		a = a + 1;
-		printf(“%d”, a);
-	}
-	a++;	
+ signed int a = -2;
+ //Single line comment
+ a++;
+ printf("%d\n", a);
+ /* Multiline
+ comment */
 }
